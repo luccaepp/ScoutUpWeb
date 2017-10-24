d6f8a3dd68a39678483b0a8b62b0443707ec0139
Patrick <trick_stival@hotmail.com>
2017-10-24 18:00:08 -0200

    Agora o escotista pode aceitar ou recusar o convite de um membro da seção

--- src/layout/Areas/PanelMembros/PanelAdicionarMembroNaSecao.vue
+++ src/layout/Areas/PanelMembros/PanelAdicionarMembroNaSecao.vue
@@ -4,19 +4,35 @@ import { mapGetters } from 'vuex'
 export default {
   props: ['convites', 'secao'],
   computed: {
-    ...mapGetters({database: 'getDatabase', usuarioDatabase: 'getUsuarioDatabase'})
+    ...mapGetters({database: 'getDatabase', usuarioDatabase: 'getUsuarioDatabase'}),
+    refSolicitacoesDaSecao(){
+      return this.database.ref('grupo')
+                          .child(this.usuarioDatabase.grupo)
+                          .child('secoes')
+                          .child(this.secao['.key'])
+                          .child('solicitacoes')
+    }
   },
   methods: {
     aceitarConvite(convite){
-      this.database.ref('usuario').child(convite.usuario.chave).update({
+      let self = this
+      this.getRefdoUsuarioDoConvite(convite).update({
         grupo: this.usuarioDatabase.grupo,
         secao: {
           chave: this.secao['.key'],
           nome: this.secao.nome
         }
-      }).then(snap => {
-        this.database.ref('/grupo/'+this.usuarioDatabase.grupo+'/secoes/'+this.secao['.key']+'/solicitacoes/'+convite['.key']).remove()
-      })
+      }).then(snap => self.removerConvite(convite))
+    },
+    removerConvite(convite){
+      let self = this
+      this.getRefdoUsuarioDoConvite(convite)
+          .child('solicitacaoDeEntradaEmSecao')
+          .remove()
+          .then(rmSnap => self.refSolicitacoesDaSecao.child(convite['.key']).remove())
+    },
+    getRefdoUsuarioDoConvite(conv){
+      return this.database.ref('usuario').child(conv.usuario.chave)
     }
   }
 }
@@ -30,21 +46,42 @@ export default {
     </div>
     <div class="panel-body">
       <div class="list-group">
-        <button @click="aceitarConvite(convite)" v-for="convite in convites" :key="convite['.key']" class="list-group-item list-group-item-info">
-          {{convite.usuario.nome}}
-          <i class="fa fa-plus text-success pull-right" aria-hidden="true"></i>
-        </button>
+        <div v-for="convite in convites" :key="convite['.key']" class="list-group-item list-group-item-info">
+          <div class="row">
+ 
+            <div class="flexboxinvitador">
+              <span class="nomeInvitador">
+                {{convite.usuario.nome}}
+              </span>
+              <button class="btn btn-danger">
+                <i class="fa fa-times" aria-hidden="true"></i>
+              </button>
+              <button class="btn btn-success" @click="aceitarConvite(convite)">
+                <i class="fa fa-plus" aria-hidden="true"></i>
+              </button>
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+.flexboxinvitador{
+  display: flex;
+  justify-content: flex-end;
+}
 .list-group-item-info{
   cursor: pointer;
   outline: none;
 }
-.list-group-item-info:hover{
-  background-color: ;
+.nomeInvitador{
+  flex-grow: 2;
+  padding: 5px;
+}
+.btn-success, .btn-danger{
+  margin-right: 5px;
 }
 </style>
