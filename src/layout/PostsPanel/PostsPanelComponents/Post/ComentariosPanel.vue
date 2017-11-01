995302503ae88f05eaa4d738ddf19315cba142a1
trickstival <trick_stival@hotmail.com>
2017-11-01 15:59:29 -0200

    Painel de comentários agora expande e diminui

--- src/layout/PostsPanel/PostsPanelComponents/Post/ComentariosPanel.vue
+++ src/layout/PostsPanel/PostsPanelComponents/Post/ComentariosPanel.vue
@@ -1,7 +1,7 @@
 <script>
 import StComentarioBox from './ComentariosPanel/ComentarioBox.vue'
 import StEscrevaUmComentarioPanelFooter from './ComentariosPanel/EscrevaUmComentarioPanelFooter.vue'
-import _ from 'lodash' 
+import _ from 'lodash'
 import {mapGetters} from 'vuex'
 import {EventBus} from '../../../../eventBus.js'
 
@@ -29,7 +29,7 @@ var vm = {
                      ? this.post.comentarios
                         ? Object.keys(this.post.comentarios).map(key => this.post.comentarios[key]).slice(0, this.limitAtual)
                         : null
-                     :null 
+                     :null
 
         }
     }
@@ -48,11 +48,13 @@ export default vm
         <div class="panel-body">
             <div class="row">
                 <div v-for="comentario in comentariosLimitados">
-                     <st-comentario-box :pathParaOPost="pathParaOPost" 
-                        :comentario="comentario"></st-comentario-box> 
+                     <st-comentario-box :pathParaOPost="pathParaOPost"
+                        :comentario="comentario"></st-comentario-box>
                 </div>
             </div>
-            <a class="pull-right text-verdinho" href="#">Mostrar mais...</a>
+            <span @click="limitAtual += 5" class="pull-right text-verdinho mostrar">Mostrar mais...</span>
+            <span v-if="limitAtual > 3" @click="limitAtual = 3"
+                  class="pull-right text-verdinho mostrar">Mostrar menos...</span>
         </div>
         <st-escreva-um-comentario-panel-footer v-if="ehDessaArea" :pathParaOPost="pathParaOPost"></st-escreva-um-comentario-panel-footer>
     </div>
@@ -63,5 +65,12 @@ export default vm
     font-size:18px;
     font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
     margin-bottom: 0;
+}
+.mostrar{
+  cursor: pointer;
+  margin: 5px;
+}
+.mostrar:hover{
+  text-decoration: underline;
 }
 </style>
